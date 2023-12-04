6df83248ed7f36acbaf45cb87eb944bc6a7988aa
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2023-12-04 13:44:01 +0900

    当たり判定

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -3,6 +3,8 @@
 // 他のJSファイルから呼び出された場合はシーンを返す
 class MyScene extends Phaser.Scene {
 
+    hanakoFlag = false;
+
     // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
     constructor() {
         super({ key: 'MyScene', active: true });
@@ -22,27 +24,32 @@ class MyScene extends Phaser.Scene {
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'sky');
-        // this.taro = this.physics.add.image(D_WIDTH/2, D_HEIGHT/2, 'taro');
         this.taro = this.physics.add.image(D_WIDTH/2, D_HEIGHT/2, 'taro');
         this.jiro = this.physics.add.image(D_WIDTH/2, D_HEIGHT/2, 'jiro');
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
         this.text2 = this.add.text(600, 400, 'MyWorld').setFontSize(20).setColor('#ff0');
 
 
+        //時間
+        this.timeCounter = 0;
+        this.leftTime = 3;
+
+
         this.helloText = this.add.text(100, 50, '');
         this.heyText = this.add.text(100, 50, '');
+
 
         this.keys = {};
         this.keys.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keys.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keys.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        this.keys.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        // this.keys.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         // this.taro.angle = 0;
         // this.player_direction = 1;
     }
 
   // 毎フレーム実行される繰り返し処理
-    update() {
+    update(time, delta) {
         // if(this.taro.y >= D_HEIGHT - 100) this.player_direction = -1
         // if (this.taro.y <= 100) this.player_direction = 1;
 
@@ -64,13 +71,23 @@ class MyScene extends Phaser.Scene {
         // this.taro.setVelocityY(50);
         let cursors = this.input.keyboard.createCursorKeys();
         if (cursors.left.isDown) {
-            this.taro.x -= 50;
-            this.jiro.x += 50;
+            this.taro.setVelocityX(-200);
+            // this.taro.x -= 50;
+            // this.jiro.x += 50;
         } else if (cursors.right.isDown) {
-            this.taro.x += 50;
-            this.jiro.x -= 50;
+            this.taro.setVelocityX(200);
+            // this.taro.x += 50;
+            // this.jiro.x -= 50;
+        }else if (cursors.up.isDown) {
+            this.taro.setVelocityY(-200);
+        }else if (cursors.down.isDown) {
+            this.taro.setVelocityY(200);
+        } else {
+            this.taro.setVelocityX(0);
+            this.taro.setVelocityY(0);
         }
 
+        //文字列の表示
         if(this.keys.keyA.isDown){
             this.helloText.setText('Hello!');
         }else if(this.keys.keyS.isDown){
@@ -78,9 +95,36 @@ class MyScene extends Phaser.Scene {
         }else if(this.keys.keyD.isDown){
             this.helloText.setText('');
             this.heyText.setText('');
-        }else if(this.keys.keyW.isDown){
-            let randx = Phaser.Math.Between(100, 400);
-            this.hanako = this.add.image(randx, 100, 'hanako');
+        }
+
+        //hanakoの表示
+        // if(this.keys.keyW.isDown){
+        //     let randx = Phaser.Math.Between(100, 400);
+        //     this.hanako = this.add.image(randx, 100, 'hanako');
+        // }
+
+        this.timeCounter += delta;
+        if(this.timeCounter > 1000) {
+            this.timeCounter = 0;
+            this.leftTime --;
+        }
+        if(this.leftTime <= 0){
+            if(this.hanakoFlag){
+                this.hanako.destroy();
+            }
+            let randx = Phaser.Math.Between(200, 400);
+            let randy = Phaser.Math.Between(100, 200);
+            // this.hanako = this.physics.add.image(randx, randy, 'hanako');
+            let hanakoGroup = this.physics.add.group();
+            this.hanako = hanakoGroup.create(randx, randy, 'hanako');
+            this.hanakoFlag = true;
+            this.leftTime = 3;
+        }
+
+        // 当たり判定
+        this.physics.add.overlap(this.taro, this.hanako, collision_detection, null, this);
+        function collision_detection() {
+            this.add.text(100, 150, '痛い！',);
         }
     }
 
